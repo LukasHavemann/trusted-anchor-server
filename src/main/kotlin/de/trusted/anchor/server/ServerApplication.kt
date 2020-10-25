dca354b37033fc9ec21a8e5535acbba5bfb469aa
Lukas Havemann <lukas.havemann@ppi.de>
2020-10-25 20:11:50 +0100

    test ci

--- src/main/kotlin/de/trusted/anchor/server/ServerApplication.kt
+++ src/main/kotlin/de/trusted/anchor/server/ServerApplication.kt
@@ -3,7 +3,6 @@ package de.trusted.anchor.server
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-
 @SpringBootApplication
 class ServerApplication
 
